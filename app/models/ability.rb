61826e6dd7d54fe804933bd7440a199de10f431e
Diego Cruz <dacruzt@outlook.com>
2021-03-17 17:18:59 -0500

    primer commit

---
+++ app/models/ability.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :manage, Task, owner_id: user.id
+    can :read, Task, participating_users: { user_id: user.id }
+      	# * si el usuario es propietario del post puede escribir notas
+    can :create, Note do |n|
+    # obtener el propietario de la tarea
+    owner_id = Task.find(n.task_id).owner_id
+    # obtener el pid del current user
+    user_id = user.id
+    # comparo si el current user es propietario de la tarea
+    owner_id == user_id
+  end
+
+end
+end
